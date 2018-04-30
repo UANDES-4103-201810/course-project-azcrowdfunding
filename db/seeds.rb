a97fbaba2d471ca0de7db46eb225be7e02aab0b7
Alfonso <ajzuniga1@miuandes.cl>
2018-04-30 22:31:36 +0100

    Added admin to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: 'Max', last_name: 'Brijido', email: 'mbrijido@miuandes.cl', admin: true, phone: '666666661', address: 'Casa de Max 69', country: 'Chile', city: 'San Bernando', password: '123456')
