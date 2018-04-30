1d99ddcaa17633323d0efaad53171ceebf1051ad
Angeles <adaldunate@miuandes.cl>
2018-04-30 19:57:54 -0300

    Admin, problems with edit others

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,48 @@ class UsersController < ApplicationController
   def show_all
     @users = User.all
   end
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Successfully created User."
+      redirect_to root_path
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    params[:user].delete(:password) if params[:user][:password].blank?
+    params[:user].delete(:password_confirmation) if params[:user][:password].blank? and params[:user][:password_confirmation].blank?
+
+    if @user.update(user_params)
+      flash[:notice] = "Successfully updated User."
+      redirect_to root_path
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:notice] = "Successfully deleted User."
+      redirect_to root_path
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   resources :categories
   resources :favorites
   resources :projects
-  devise_for :users
+  devise_for :users, :path_prefix => 'd'
   resources :users, only: [:show]
   get 'my_projects', to: 'projects#show_my'
   get 'show_all', to: 'users#show_all'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#index'
+  resources :users, :controller => "users"
+
 end
