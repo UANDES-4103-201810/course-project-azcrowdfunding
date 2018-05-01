9c4675917a27fce27fc5240aa2bc087e487f84a6
Angeles <adaldunate@miuandes.cl>
2018-05-01 17:38:51 -0300

    Favorites done

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   get 'my_projects', to: 'projects#show_my'
   get 'show_all', to: 'users#show_all'
+  get 'show_my_fav', to: 'favorites#show_my_fav'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#index'
